f163406750d019fea5f07fdfa6e52f3c6bbf9ffe
jellaharikrishna <rk40665@gmail.com>
2025-02-11 20:03:22 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: "https://fullstack-tenant-management-system-1wmb.onrender.com", // Removed trailing slash
+    origin: "https://fullstack-tenant-management-system-2fyp.vercel.app", // Removed trailing slash
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
     credentials: true // Include credentials if required
